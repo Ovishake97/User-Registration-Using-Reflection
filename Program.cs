8c04a5daa9946154c548b44f8178a5f6f9499d1e
Abhishek Kumar Sharma <theabhishek.aks22@gmail.com>
2020-10-15 20:53:25 +0530

    [Abhishek] Add . Added to the UC6 branch

--- Program.cs
+++ Program.cs
@@ -10,10 +10,14 @@ namespace UserRegistrationReflection
             Console.WriteLine("Welcome to the User Registration Portal");
             Console.WriteLine("------------------------------");
             const string EMAIL_PATTERN = "[a-zA-Z0-9]+([+-_.][a-zA-Z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]+([.][a-zA-Z]{2})*$";
+            const string PASSWORD_PATTERN = "^(?=.*[A-Z])(?=.*[a-z]).{8,}$";
             Console.WriteLine("Enter the email of the user");
             string email = Console.ReadLine();
             string result = UserValidation.ValidateDetails(email, EMAIL_PATTERN);
             Console.WriteLine(result);
+            Console.WriteLine("Enter the password of the user");
+            string password = Console.ReadLine();
+            string resultTwo = UserValidation.ValidateDetails(password, PASSWORD_PATTERN);
             Console.ReadKey();
         }
     }
