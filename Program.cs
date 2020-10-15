812f4f8567f174d3dd8c119e05d2d3f2ca284e1e
Abhishek Kumar Sharma <theabhishek.aks22@gmail.com>
2020-10-15 21:05:15 +0530

    [Abhishek] Add . Added to the UC8 branch

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ namespace UserRegistrationReflection
             Console.WriteLine("Welcome to the User Registration Portal");
             Console.WriteLine("------------------------------");
             const string EMAIL_PATTERN = "[a-zA-Z0-9]+([+-_.][a-zA-Z0-9]+)*[@][a-zA-Z0-9]+[.][a-zA-Z]+([.][a-zA-Z]{2})*$";
-            const string PASSWORD_PATTERN = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{8,}$";
+            const string PASSWORD_PATTERN = "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?!.*[^0-9a-zA-Z].*[^0-9a-zA-Z]).{8,}$";
             Console.WriteLine("Enter the email of the user");
             string email = Console.ReadLine();
             string result = UserValidation.ValidateDetails(email, EMAIL_PATTERN);
@@ -18,6 +18,7 @@ namespace UserRegistrationReflection
             Console.WriteLine("Enter the password of the user");
             string password = Console.ReadLine();
             string resultTwo = UserValidation.ValidateDetails(password, PASSWORD_PATTERN);
+            Console.WriteLine(resultTwo);
             Console.ReadKey();
         }
     }
